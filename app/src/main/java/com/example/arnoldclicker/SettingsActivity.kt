faf3a9b34145a4f2fbce87574c8e5155f9f7c0fe
Amin405 <amin_soussi_github@hotmail.com>
2022-12-06 14:43:38 +0100

    ADD: implement settings

--- app/src/main/java/com/example/arnoldclicker/SettingsActivity.kt
+++ app/src/main/java/com/example/arnoldclicker/SettingsActivity.kt
@@ -1,11 +1,47 @@
 package com.example.arnoldclicker
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setupLoadButton()
+        setupExitButton()
+        setupSaveButton()
+    }
+
+    fun setupSaveButton() { //TODO
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(
+                applicationContext,
+                "State has been saved",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    fun setupLoadButton() {//TODO
+        var button = findViewById<Button>(R.id.button3)
+        button.setOnClickListener {
+            Toast.makeText(
+                applicationContext,
+                "State has been loaded",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    fun setupExitButton() {
+        var exitButton = findViewById<Button>(R.id.exitButton)
+        exitButton.setOnClickListener {
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
     }
 }
